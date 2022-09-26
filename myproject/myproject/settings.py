b5f7b13f4cd8d55af025dd41a94296f0cfd7e0ae
clamprou <79994264+clamprou@users.noreply.github.com>
2022-09-26 20:54:16 +0300

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -143,6 +143,7 @@ AUTH_USER_MODEL = 'users.Myuser'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = BASE_DIR + 'static' 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
